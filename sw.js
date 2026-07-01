2cc1780f70f46ceccc04345763a968cb51c74cc1
mhd haza <35713532+agiBalaja@users.noreply.github.com>
2026-07-01 09:29:01 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'Indo News';
+const urlsToCache = [
+  '/Landing-page/',
+  '/Indo News Video Generator - Edisi Gabungan/index.html',
+  // Tambahkan file lain yang diperlukan (CSS, JS, gambar)
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Ambil dari cache saat offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
